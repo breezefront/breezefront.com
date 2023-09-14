c371589606d752a8477b439b0b3baf9b358bdafe
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2023-09-14 17:18:02 +0300

    Show copy button on focus

--- assets/scripts.js
+++ assets/scripts.js
@@ -67,7 +67,7 @@ document.addEventListener('turbolinks:load', () => {
   document.querySelectorAll('div.highlighter-rouge:not(.nocopy)').forEach(div => {
     div.classList.add('group');
     div.insertAdjacentHTML('beforeend', `
-      <button data-copy-code class="copy absolute top-2 right-2 text-white opacity-0 h-6 w-6 group-hover:opacity-30">
+      <button data-copy-code class="copy absolute top-2 right-2 text-white opacity-0 h-6 w-6 group-hover:opacity-30 focus-visible:opacity-30 outline-white">
         <svg xmlns="http://www.w3.org/2000/svg" class="absolute left-0 top-0 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
         </svg>
